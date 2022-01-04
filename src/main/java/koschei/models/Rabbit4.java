9ba5ab307db4e5f15274ffed9ef10267d495340b
Irina_Kharitonova <irina11072005@yandex.ru>
2022-01-04 15:28:59 +0300

    first commit

---
+++ src/main/java/koschei/models/Rabbit4.java
@@ -0,0 +1,15 @@
+package koschei.models;
+
+public class Rabbit4 {
+
+    private final Duck5 duck;
+
+    public Rabbit4(Duck5 duck) {
+        this.duck = duck;
+    }
+
+    @Override
+    public String toString() {
+        return ", в зайце утка " + duck.toString();
+    }
+}
